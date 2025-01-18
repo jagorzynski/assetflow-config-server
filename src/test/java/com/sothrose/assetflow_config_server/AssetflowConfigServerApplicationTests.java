33ac4c42dd59bec01768c590e6516b32d886fb12
Gorzynski Jakub <gorzynski.jakub@protonmail.com>
2025-01-18 09:46:08 +0100

    init commit

---
+++ src/test/java/com/sothrose/assetflow_config_server/AssetflowConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sothrose.assetflow_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetflowConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
